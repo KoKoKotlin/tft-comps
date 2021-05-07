ca486476e57b75a7c3aa66d433f3fdcabadaf120
KoKoKotlin <labruzzler@gmail.com>
2021-05-07 22:02:10 +0200

    Updates

---
+++ precache-manifest.ce1170d6d3fa8a76c7c6d40ee62a852a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8596d869c9dfcbdbc40b6ca17299c19b",
+    "url": "/tft-comps/index.html"
+  },
+  {
+    "revision": "40ce5727aa176ebd3a38",
+    "url": "/tft-comps/static/css/main.14366e51.chunk.css"
+  },
+  {
+    "revision": "63daeeed1686697b522c",
+    "url": "/tft-comps/static/js/2.eab67065.chunk.js"
+  },
+  {
+    "revision": "83e93b2c4ba8ecf8fa8a14c5dd987cee",
+    "url": "/tft-comps/static/js/2.eab67065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40ce5727aa176ebd3a38",
+    "url": "/tft-comps/static/js/main.e2d67c3b.chunk.js"
+  },
+  {
+    "revision": "bff724cd3fe62bc90eb1",
+    "url": "/tft-comps/static/js/runtime-main.642bbe48.js"
+  }
+]);
